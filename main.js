abfb27be7d23f85fa85334060fb56428104b03bd
Chris Madrigal <crismas07@gmail.com>
2014-09-01 12:48:31 -0700

    test date

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
  var today = new Date()
    , endDate = new Date("2017 jan 17")
    , diff =  endDate.getTime() - today.getTime()
-   , DaysTill = Math.floor(diff / (1000 * 60 * 60 * 24));
+   , DaysTill = Math.floor(diff / (1000 * 60 * 60 * 24))
+   , output = []
+   ;
 
-  console.log(DaysTill);
+   sNumber = DaysTill.toString();
+
+  for (var i = 0, len = sNumber.length; i < len; i += 1) {
+    output.push(+sNumber.charAt(i));
+  }
+
+  console.log(output);
